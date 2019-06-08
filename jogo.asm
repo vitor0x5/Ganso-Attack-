df32ce5b79722d3a5ae5580b332dc9bac3c43313
Vitor Hugo Guilherme <vitorhugo.glh@gmail.com>
2019-06-08 10:53:00 -0300

    Menu inicial funcionando

---
+++ jogo.asm
@@ -0,0 +1,86 @@
+; Implementa��o do jogo Ganso Attack!! em assembly pelos alunos
+; Matheus de Brito Soares Porto RA: 744348
+; Vitor Hugo Guilherme          RA: 744359
+; Da disciplina Arquitetura e Organiza��o de Computadores 2 - DC- UFSCar
+
+INCLUDE Irvine32.inc
+
+.data
+
+outHandle    DWORD ? 
+scrSize COORD <85,50>
+
+; Logo do jogo
+logo BYTE "         ____                            _   _   _             _    _ _ ",0ah, 0dh  
+	 BYTE "	 / ___| __ _ _ __  ___  ___      / \ | |_| |_ __ _  ___| | _| | |",0ah, 0dh  
+	 BYTE "	| |  _ / _` | '_ \/ __|/ _ \    / _ \| __| __/ _` |/ __| |/ / | |",0ah, 0dh  
+	 BYTE "	| |_| | (_| | | | \__ \ (_) |  / ___ \ |_| || (_| | (__|   <|_|_|",0ah, 0dh  
+	 BYTE "	 \____|\__,_|_| |_|___/\___/  /_/   \_\__|\__\__,_|\___|_|\_(_|_)",0ah, 0dh, 0 
+
+;MENU
+ menu   BYTE "	    Selecione uma dificuldade para jogar:",0ah, 0dh, 0ah
+		BYTE "				1 - Facil",0ah, 0dh
+		BYTE "				2 - Dificil",0ah, 0dh
+        BYTE "				ESC - SAIR",0ah, 0dh
+		BYTE "				",0
+                                                                  
+ 
+.code
+
+;==================Desenha o menu inicial ==========================
+;Recebe: nda
+;Retorna: Desenho do menu na tela
+;===================================================================
+DesenhaMenu PROC
+	mov  eax, red
+	call SetTextColor
+	mov dl, 1
+	mov dh, 8
+	call GotoXY
+	mov edx, OFFSET logo
+    call WriteString
+
+	mov eax, white
+	call SetTextColor 
+	mov dh, 16
+	call GotoXY
+    mov edx, OFFSET menu   
+	call WriteString  
+	ret
+DesenhaMenu ENDP
+;=================================================
+
+main PROC
+	INVOKE GetStdHandle,STD_OUTPUT_HANDLE 
+	mov outHandle, eax
+	INVOKE SetConsoleScreenBufferSize,outHandle,scrSize
+	call Clrscr
+	
+	;Desenha menu
+	call DesenhaMenu  
+
+	;Esperando tecla ser pressionada
+EsperandoTecla:
+		mov  eax,50          ; sleep, to allow OS to time slice
+		call Delay           ; (otherwise, some key presses are lost)
+		call ReadKey         ; look for keyboard input 
+		push edx
+
+	.IF al == "1"
+		;TODO jogo fácil
+		call WriteChar
+		jmp SAIR
+	.ELSEIF al == "2"
+		;TODO jogo dificil
+		call WriteChar
+		jmp SAIR
+	.ELSEIF al == VK_ESCAPE
+		exit
+	.ENDIF
+	
+	jmp   EsperandoTecla    ; nenhuma tecla válida pressionada, tenta novamente
+SAIR:
+	call ReadChar
+	exit
+main ENDP
+END main
